c92e67020fd557b3a651c50766adfa5abf8ea039
Matthew McGowan <mmcgowan280@gmail.com>
2018-03-03 14:54:44 -0800

    Added basic function to import parental genotypes

--- R/LitterSim.R
+++ R/LitterSim.R
@@ -1,11 +1,17 @@
 LitterSim.import <- function(importpath)
 {
-  parent_pheno <- read.table(importpath, header = T, sep = "\t")
+  parent_pheno <- read.table(importpath, header = T, sep = ",")
   parent_geno <- data.frame()
   progeny_geno <- data.frame()
   progeny_pheno <- data.frame()
 
-  return(list(parent_pheno, parent_geno, progeny_geno, progeny_pheno))
+  combined_frames <- list(parent_pheno, parent_geno, progeny_geno, progeny_pheno)
+  names(combined_frames) <- c("parent_pheno", "parent_geno", "progeny_geno", "progeny_pheno")
+  return(combined_frames)
 }
 
-LitterSim.
+LitterSim.g2p <- function(LitterSim, trait_rules)
+{
+  # Need to determine how to store and access trait rules
+
+}
